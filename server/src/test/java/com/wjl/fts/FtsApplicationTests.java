33f3a876a04c9f215720fbbb450ec6966437e333
wlonestar <68649094+wlonestar@users.noreply.github.com>
2023-05-16 13:45:24 +0000

    create from template

---
+++ server/src/test/java/com/wjl/fts/FtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjl.fts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
